973d3f69648b47fb7cfc309a6d386a71553a531c
Gitratter <23t3037@stu.meisei-u.ac.jp>
2026-05-17 17:55:38 +0900

    Update RAM.v

--- sources_1/new/RAM.v
+++ sources_1/new/RAM.v
@@ -1,1 +1,17 @@
+module RAM (
+  input clk,
+  input we, //write enable
+  input [7:0] addr, //addres
+  input [7:0] din, //data in
+  output reg [7:0] dout //data out
+);
+  reg [7:0] mem [0:255];
+
+  always @(posedge clk)begin
+    if(we)begin
+      mem[addr] <= din;
+    end
+    dout <= mem[addr];
+  end
+endmodule
 
