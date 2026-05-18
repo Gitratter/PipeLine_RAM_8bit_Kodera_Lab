657d2db1a7eb070db28153e0b4f74535cb1c4916
Gitratter <23t3037@stu.meisei-u.ac.jp>
2026-05-18 17:12:33 +0900

    Update ALU.v

--- sources_1/new/ALU.v
+++ sources_1/new/ALU.v
@@ -5,5 +5,5 @@ module ALU(ALUsel, select_data, im, ALU_data, carry);
     output [7:0]ALU_data;
     output carry;
 
-    assign {carry, ALU_data} = ALUsel?(select_data-im):(select_data+im);
+    assign {carry, ALU_data} = ALUsel ? (select_data-im):(select_data+im);
 endmodule
