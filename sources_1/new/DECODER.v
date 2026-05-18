ebceea4c67ae5c05d865f1f7c2819e924e7ae17a
Gitratter <23t3037@stu.meisei-u.ac.jp>
2026-05-18 17:14:23 +0900

    Update DECODER.v

--- sources_1/new/DECODER.v
+++ sources_1/new/DECODER.v
@@ -1,4 +1,3 @@
-//Decoder
 module DECODER(opcode, carry, instr);
 	input [4:0]opcode;
     input carry;
@@ -21,7 +20,7 @@ module DECODER(opcode, carry, instr);
             5'b01010 : instr = 11'b001101x_0_0_00;  //OUT A 追加
             5'b01001 : instr = 11'b011101x_0_0_00;  //OUT B
             5'b01111 : instr = 11'b111110x_0_0_00;  //JMP Im
-			5'b01110 : instr = Carry? 11'bxx1111x_0_0_00 : 11'b111110x_0_0_00;  //JNC Im
+			5'b01110 : instr = carry ? 11'bxx1111x_0_0_00 : 11'b111110x_0_0_00;  //JNC Im
             5'b01100 : instr = 11'b111111x_0_0_00;  //NOP
 			5'b10000 : instr = 11'b001111x_1_0_00;  //LOAD A,[Im]
 			5'b10001 : instr = 11'b011111x_1_0_01;  //LOAD B,[Im]
@@ -33,4 +32,3 @@ module DECODER(opcode, carry, instr);
         endcase
     end
 endmodule
-
